fd66c8bbdc142887165683c9920d3d6ae7785b37
ISC_011 <ISC_011@LAPTOP-98FM46E8.mshome.net>
2021-01-27 15:35:49 +0100

    test

--- extranet/src/main/java/com/gruppo/isc/extranet/controller/ProvaController.java
+++ extranet/src/main/java/com/gruppo/isc/extranet/controller/ProvaController.java
@@ -3,7 +3,6 @@ package com.gruppo.isc.extranet.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
