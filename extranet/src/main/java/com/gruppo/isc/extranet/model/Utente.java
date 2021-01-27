5edb91380b35e5f70cafc62df10d29dbf8564b7f
Alessio <Alessio@DESKTOP-138E0CL.mshome.net>
2021-01-27 10:14:10 +0100

    inizio

---
+++ extranet/src/main/java/com/gruppo/isc/extranet/model/Utente.java
@@ -0,0 +1,46 @@
+package com.gruppo.isc.extranet.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="utente")
+@Data
+public class Utente implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6544359484876967725L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_utente")
+	private int id;
+	
+	@Column(name = "username")
+	private String username;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "stato")
+	private int stato;
+	
+	@Column(name = "primo_accesso")
+	private int primo_accesso;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_id_gruppo", referencedColumnName = "id")
+	private Gruppo gruppo;
+}
