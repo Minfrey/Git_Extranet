e2dcdb8c3b54307035bb16535b581d87d25b20e0
Fabiocoluccia93 <fabio.coluccia93@gmail.com>
2021-02-24 14:27:08 +0100

    getActive

--- extranet/src/main/java/com/gruppo/isc/extranet/repository/RisorseRepo.java
+++ extranet/src/main/java/com/gruppo/isc/extranet/repository/RisorseRepo.java
@@ -7,6 +7,7 @@ import com.gruppo.isc.extranet.model.Risorse;
 public interface RisorseRepo 
 {
 	public List<Risorse> getRisorseList();
+	public List<Risorse> getRisorseListActive();
 	public Risorse setRisorse(Risorse r);
 	public Risorse modRisorse(Risorse r);
 	public void schedulRisorse(String a);
