5bbc52bce7dac417120437e44c297fb428b4651a
alessiofor9 <fornaro47@gmail.com>
2021-03-03 12:42:29 +0100

    metodo per prendere tutti i tipi di avanzamento

--- extranet/src/main/java/com/gruppo/isc/extranet/repository/TipoAvanzamentoRepoImp.java
+++ extranet/src/main/java/com/gruppo/isc/extranet/repository/TipoAvanzamentoRepoImp.java
@@ -1,9 +1,29 @@
 package com.gruppo.isc.extranet.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.gruppo.isc.extranet.model.TipoAvanzamento;
 
 @Repository
 public class TipoAvanzamentoRepoImp implements TipoAvanzamentoRepo
 {
+	@Autowired
+	EntityManager em;
+	
+	@Override
+	public List getAllTipiAvanzamento() {
+		List<TipoAvanzamento> tipi = new ArrayList<TipoAvanzamento>();
+		Query q = em.createQuery("select t from TipoAvanzamento t");
+		tipi = q.getResultList();
+		
+		return tipi;
+	}
 
 }
